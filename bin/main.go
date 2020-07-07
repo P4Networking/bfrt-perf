a277b285c6a278b2c3bd1f19e0e5b1104a858691
Yi Tseng <yi@opennetworking.org>
2020-07-07 11:40:55 -0700

    Update style and license header

--- bin/main.go
+++ bin/main.go
@@ -1,19 +1,6 @@
-/*
- * Copyright 2020-present Brian O'Connor
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+// Copyright 2020-present Brian O'Connor
+// Copyright 2020-present Open Networking Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package main
 
@@ -59,11 +46,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	//config, err := client.GetForwardingPipelineConfig()
-	//if err != nil {
-	//	panic(err)
-	//}
 
 	// Set up write tracing for test
 	writeTraceChan := make(chan p4rt.WriteTrace, 100)
--- p4rt/pipeline_stratum_bf.go
+++ p4rt/pipeline_stratum_bf.go
@@ -1,21 +1,7 @@
-// +build tofino
-
-/*
- * Copyright 2020-present Brian O'Connor
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+// +build stratum-bf
+// Copyright 2020-present Brian O'Connor
+// Copyright 2020-present Open Networking Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 // To include this file, use go build -tags tofino
 
